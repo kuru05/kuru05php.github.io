9242f74dcb28bef4c2aa6c2eeca4e51ba018245b
kuru05 <142448890+kuru05@users.noreply.github.com>
2024-02-08 12:10:42 +0100

    Add files via upload

---
+++ Core/Logic/Menu.php
@@ -0,0 +1,16 @@
+<?php
+$index= "index.php";
+$_index="../index.php";
+$seasons="fr/seasons.html";
+$_seasons="seasons.html";
+$about="fr/about.html";
+$_about="about.html";
+
+function fonction_header($arg_1, $arg_2,  $arg_3){
+    echo "<li><a href=" . $arg_1 . "> Home</a></li>\n";
+    echo "<li><a href=" . $arg_2 . "> Home</a></li>\n";
+    echo "<li><a href=" . $arg_3 . "> Home</a></li>";
+}
+
+
+
